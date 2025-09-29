83fcf61034ace509e45a8899458e597514614dec
ayanhalder27 <ayanhalder2705@gmail.com>
2025-09-30 02:48:31 +0600

    blank profile pic added

--- Hospital Management System/Properties/Resources.Designer.cs
+++ Hospital Management System/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Hospital_Management_System.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap blank_profile_picture_973460_1920 {
+            get {
+                object obj = ResourceManager.GetObject("blank-profile-picture-973460_1920", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap calendar {
             get {
                 object obj = ResourceManager.GetObject("calendar", resourceCulture);
