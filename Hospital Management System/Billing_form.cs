5162f3e9a1a5b116fed25c3d8a5002d38830de1b
Tamjid Niloy <tamjidniloy22@gmail.com>
2025-10-04 19:49:27 +0600

    Problem Solved

--- Hospital Management System/Billing_form.cs
+++ Hospital Management System/Billing_form.cs
@@ -45,7 +45,7 @@ namespace Hospital_Management_System
                                 PatientName = p.FullName,
                                 PatientPhone = p.PhoneNumber,
                                 DoctorName = d.FullName,
-                                Appointment_Date = b.BillDate,
+                                BillDate = b.BillDate,
                                 Amount = (decimal)b.Amount,
                                 Status = b.Status
                             };
@@ -117,7 +117,7 @@ namespace Hospital_Management_System
         public string PatientName { get; set; }
         public string PatientPhone { get; set; }
         public string DoctorName { get; set; }
-        public DateTime Appointment_Date { get; set; }
+        public DateTime BillDate { get; set; }
         public decimal Amount { get; set; } // doctor visit fee
         public string Status { get; set; }  // Paid/Unpaid
     }
